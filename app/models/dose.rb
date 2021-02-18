95936c7f1e7338c7957c94ba5cc542d6eda98700
ben <benjaminskingsley@gmail.com>
2021-02-18 11:56:40 +0000

    Created models and dependencies

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  belongs_to :cocktail
+  belongs_to :ingredient
+
+  validates :description, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
+end
