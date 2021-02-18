ebe54743e1df04b0fb7d2aa8e4d0ca69baecac60
ben <benjaminskingsley@gmail.com>
2021-02-18 12:56:28 +0000

    Finished controllers, views and forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails,  only: [:index, :new, :create, :show] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [ :destroy ]
 end
