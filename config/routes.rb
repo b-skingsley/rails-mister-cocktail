c70f850b5f3ec97a71a84ac61a814aa1cebe2c39
ben <benjaminskingsley@gmail.com>
2021-03-30 19:42:43 +0100

    Added root for index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'cocktails#index'
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
